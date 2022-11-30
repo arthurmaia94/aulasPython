941fad8ee3338408286a92a87c030e8f3d19a753
Arthur Maia <64670862+arthurmaia94@users.noreply.github.com>
2022-11-29 22:58:20 -0300

    Atividade das Bandeiras
    Pequeno programa para desenhar a bandeira do japão com a biblioteca PIL

---
+++ atividadesBandeiras2.py
@@ -0,0 +1,51 @@
+# Importando a biblioteca PIL
+from PIL import Image
+
+# Importando a biblioteca OS
+import os
+
+# Constantes da biblioteca OS para dados de entrada e saida
+INPUT_FOLDER="input"
+OUTPUT_FOLDER="output"
+
+# Inicio da função para retornar o diretório temporario da imagem
+def in_path(filename):
+	return os.path.join(INPUT_FOLDER, filename)
+
+# inicio da função para desenhar a imagem
+def triangle(size):
+	white = (255,255,255) # atribuição o RGB, equivalente a cor branca para variavel
+	black = (0,0,0) # atribuição o RGB, equivalente a cor preta para variavel
+	# Insância da classe Image (PIL) recebendo como parametro as variaveis para dimensçoes e cor branca de fundo
+	image = Image.new("RGB", (size, size), white)
+	
+	# Loop reponsavel pelo desenho da area que devera ser impressa na cor preta
+	for x in range(size):
+		for y in range(size):
+			if x < y:
+				image.putpixel((x, y), black)
+	return image
+
+# função para desenha a bandeira japonesa
+def bandeira_japao(height):
+	width = 3*height//2
+	WHITE = (255, 255, 255)
+	RED = (173, 35, 35)
+	r = 3*height//10
+	c = (width//2, height//2)
+	
+	# Insância da classe Image (PIL) recebendo como parametro as variaveis para dimensçoes e cor branca
+	image = Image.new("RGB", (width, height), WHITE)
+	
+	# Loop para calcular os raios do circulo que deverá ser impresso
+	for x in range(c[0]-r, c[0] + r):
+		for y in range(c[1]-r, c[1] + r):
+			if (x-c[0])**2 + (y-c[1])**2 <= r**2:
+				image.putpixel((x, y), RED)
+	return image
+
+# Checagem de escopo
+# Usando a varivel nativa __name__ para validar se retornar o módulo atual
+if __name__ == "__main__":
+	bandeira = bandeira_japao(700)
+	bandeira.show()
